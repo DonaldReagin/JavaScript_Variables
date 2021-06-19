c7033a29ea671eeaa0788509cf4a20826f6801a6
Donald Reagin <don.rage616@gmail.com>
2021-06-19 08:30:48 -0400

    Completed exercises

--- app.js
+++ app.js
@@ -5,6 +5,33 @@ console.log(
 
 // Exercise 1
 
+const firstName = "Donnie";
+
+let lastName = "Reagin";
+
+var age = 29;
+
+console.log(firstName)
+
+console.log(lastName)
+
+console.log(age)
+
 // Exercise 2
 
+let fullName = firstName + " " + lastName;
+let templateName = `${firstName} ${lastName}`;
+
+console.log(fullName)
+console.log(templateName);
+
+
+
 // Exercise 3
+
+let city = "Atlanta"
+let passTime = "Hockey"
+
+let myStory = ` Hello! My name is ${fullName}. I live in ${city}. I like ${passTime}.`;
+
+console.log(myStory)
